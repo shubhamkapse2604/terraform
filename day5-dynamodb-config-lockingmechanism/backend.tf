a873d31dfd40b816e3e95a0a8f56f87d38b9b555
shubham <mydekstop2604@gmail.com>
2024-09-05 13:02:46 +0530

    new experiment1

--- day5-dynamodb-config-lockingmechanism/backend.tf
+++ day5-dynamodb-config-lockingmechanism/backend.tf
@@ -3,8 +3,8 @@ terraform {
     bucket = "mybucket123-statefile-conf"
     key = "dev/terraform.tfstate"
     region = "us-east-2"
-   # dynamodb_table = "terraform-state-lock-dynamo"  # DynamoDB table used for state locking, note: first run "day-5-resources-s3-dynamodb-for-statefile" to create s3 and dynamodb resources
-   # encrypt = true      # Ensures the state is encrypted at rest in S3
+    dynamodb_table = "terraform-state-lock-dynamo1"  # DynamoDB table used for state locking, note: first run "day-5-resources-s3-dynamodb-for-statefile" to create s3 and dynamodb resources
+    encrypt = true      # Ensures the state is encrypted at rest in S3
 
   }
 }
