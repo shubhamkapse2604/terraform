06d659282336c7ad44ec450cdd84370eaf50992a
shubhamkapse2604 <140498733+shubhamkapse2604@users.noreply.github.com>
2025-06-25 12:40:12 +0530

    Update main.tf

--- day1/main.tf
+++ day1/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "name-reffer-github-their-own" {
+resource "aws_instance" "name-reffer-github" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = var.key_name
